1868233713911467c97f08677b3451dc524e2fe0
Jeremy Nash <jeremynash@67-194-196-152.wireless.umnet.umich.edu>
2013-04-03 17:38:15 -0400

    Added Freivald and Cholesky

--- LinearAlgebra/matrix.py
+++ LinearAlgebra/matrix.py
@@ -23,7 +23,14 @@ class Matrix:
         self.m[x][y] = value
 
 #def diag(x):
-    
+
+def create_matrix(arr):
+    m = Matrix(len(arr), len(arr[0]))
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr[0])):
+           m[i,j] = arr[i][j]
+    return m
+
 def add(A, B):
     assert(A.rows == B.rows)
     assert(A.cols == B.cols)
@@ -77,7 +84,19 @@ def transpose(A):
     return B
 
 def cholesky(A):
-    pass
+    # A must be a symmetric matrix with real entries
+    # This is the Cholesky-Banachiewicz variant that computes row-by-row
+    L = Matrix(A.rows,A.cols)
+    for i in range(0,A.rows):
+        for j in range(0,i+1):
+            l_sum = 0
+            for k in range(0,j):
+                l_sum += L[i,k] * L[j,k]
+            if i==j:
+                L[i,j] = math.sqrt(A[j,j] - l_sum)
+            else:
+                L[i,j] = 1.0 / L[j,j] * (A[i,j] - l_sum)
+    return L
 
 def norm(v):
     # Must be a column vector
@@ -110,25 +129,56 @@ def vonMises(A):
         b = normalize(b)
     return b
 
+def freivald(A,B,C):
+    # Randomized algorithm that verifies if A*B=C. Runs in O(n^2)
+    # Always returns yes if A*B=C, returns 'yes' on A*B!=C with probability 1/2
+    import random
+
+    # Create random nx1 0/1 vector
+    r = Matrix(B.rows, 1)
+    for i in range(0, B.rows):
+        r[i,0] = random.randint(0,1)
+    Br = mult(B,r)
+    ABr = mult(A, Br)
+    Cr = mult(C, r)
+    P = subtract(ABr, Cr)
+    for i in range(0, B.rows):
+        if P[i,0] != 0:
+            return False
+    return True
+
 if __name__ == "__main__":
-    #m = Matrix(5,5)
-    #n = Matrix(5,5)
-    #m[1,1] = 5.0
-    #m[0,2] = 8.0
-    #m[3,4] = 9.0
-
-    #n[1,1] = 6.0
-    #n[2,0] = 7.0
-    #print m
-    #print n
-    #print add(m,n)
-    #print mult(m,n)
-    #print transpose(m)
-
-    p = Matrix(2,2)
-    p[0,0] = 10
-    p[0,1] = 1
-    p[1,0] = 8
-    p[1,1] = 1
+    #vonMises Test
+    p = create_matrix([[10,1],[8,1]])
     print p
     print vonMises(p)
+
+    # Cholesky Test
+    m = create_matrix([[8,3,2],[3,7,1],[2,1,9]])
+    print m
+    chol = cholesky(m)
+    print "My Cholesky decomposition:"
+    print chol
+    chol_ans = create_matrix([[2.82843, 0.00000, 0.00000],[1.06066, 2.42384, 0.00000], [0.70711, 0.10314, 2.91365]])
+    print "MATLAB Cholesky decomposition:"
+    print chol_ans
+    #d = subtract(chol_ans, chol)
+    #print d
+
+    # Freivald Test
+    n = create_matrix([[5,6,8],[-4,5,7],[-4,-9,-6]])
+    p = mult(m,n)
+    #print "Multiplied matrix:"
+    #print p
+    print "Test correct multiplication"
+    print freivald(m,n,p)
+    print freivald(m,n,p)
+    print freivald(m,n,p)
+
+    print "Test incorrect multiplication"
+    p[2,2] = 0
+    print freivald(m,n,p)
+    print freivald(m,n,p)
+    print freivald(m,n,p)
+
+
